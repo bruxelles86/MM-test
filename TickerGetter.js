9f7d47039e6c3a9506c231525460cefa838b7df5
Will Gant <w_gant@yahoo.co.uk>
2017-09-13 21:22:16 +0200

    Refactors TickerGetter into smaller functions

--- TickerGetter.js
+++ TickerGetter.js
@@ -4,8 +4,21 @@ function TickerGetter() {
   this.uri = 'mongodb://mm_recruitment_user_readonly:rebelMutualWhistle@ds037551.mongolab.com:37551/mm-recruitment'
 }
 
-TickerGetter.prototype.getTicker = function(companyName) {
+TickerGetter.prototype.getTicker = function(inputName) {
   var uri = this.uri
+  _this = this
+
+  return new Promise(function(resolve, reject) {
+    _this.dbConnect(uri)
+    .then(db => _this.extractTicker(db, inputName))
+    .catch((err) => console.log(err))
+    .then(function(tickerCode) {
+      resolve(tickerCode)
+    })
+  })
+}
+
+TickerGetter.prototype.dbConnect = function(uri) {
   return new Promise(function(resolve, reject) {
     MongoClient.connect(uri, (err, db) => {
       if (err) {
@@ -14,18 +27,18 @@ TickerGetter.prototype.getTicker = function(companyName) {
         resolve(db);
       }
     })
-  }).then(function(db) {
-    return new Promise(function(resolve, reject) {
-      db.collection('company').find().toArray(function(err, companies) {
-        company = companies.find(function(company) {
-          return company.name.toLowerCase().includes(companyName.toLowerCase())
-        })
-        resolve(company.tickerCode)
-      })
-    })
   })
-  .catch((err) => reject(err))
 }
 
+TickerGetter.prototype.extractTicker = function(db, inputName) {
+  return new Promise(function(resolve, reject) {
+    db.collection('company').find().toArray(function(err, companies) {
+      company = companies.find(function(company) {
+        return company.name.toLowerCase().includes(inputName.toLowerCase())
+      })
+      resolve(company.tickerCode)
+    })
+  })
+}
 
 module.exports = TickerGetter;
