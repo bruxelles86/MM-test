f7e76a8b321dc88b894376c0f9a92b4224afd321
Will Gant <w_gant@yahoo.co.uk>
2017-09-14 15:00:30 +0200

    Adds empty /company post route to server.js

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 app.set('view engine', 'ejs')
 
 app.listen(3000, function() {
-  console.log('listening on 3000')
+  console.log('listening on port 3000')
 })
 
 app.get('/', function(req, res) {
   res.render('index.ejs')
 })
+
+app.post('/company', function(req,res){
+
+});
 
 module.exports = app;
