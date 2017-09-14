4eb49717f0f058c5eb9ff121505e4262da4081a1
Will Gant <w_gant@yahoo.co.uk>
2017-09-14 16:30:46 +0200

    Adds failing test for card to contain two stories

--- test/web-app.js
+++ test/web-app.js
@@ -25,7 +25,7 @@ var assert = require("chai").assert,
         });
       }, 2500)
 
-      it("shows the company name on next screen after a search", done => {
+      it("shows the company ticker and price on next screen after a search", done => {
         browser.visit(uri, () => {
           browser.fill('content', 'google');
           browser.pressButton('Search').then(() => {
@@ -41,6 +41,20 @@ var assert = require("chai").assert,
           browser.fill('content', 'google');
           browser.pressButton('Search').then(() => {
           browser.assert.element('[id="card"]')
+        })
+        .catch((err) => console.log(err))
+        .then(done, done);
+        });
+      }, 2500)
+
+      it("displays two stories in company card after a search", done => {
+        var story1 = "Google has some concerns to address the balance of this year, and beyond. Over the long run, the consensus analyst recommendation for Google as a 'strong buy' is warranted as the company continues driving a healthy double-digit top line growth. But that doesn't mean there won't be a hurdle, or three, to overcome along the way."
+        var story2 = "Investors were encouraged by a healthy gain in the number of people looking at Google's ads, even as the average prices for those marketing messages extended a three-and-half year slump. The market also had been bracing for more disappointing numbers, triggering a 'relief rally' when the results weren't as bad as feared, BGC Partners analyst Colin Gillis said."
+        browser.visit(uri, () => {
+          browser.fill('content', 'google');
+          browser.pressButton('Search').then(() => {
+          browser.assert.text('#story1', story1)
+          browser.assert.text('#story2', story2)
         })
         .catch((err) => console.log(err))
         .then(done, done);
